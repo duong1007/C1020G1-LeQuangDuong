3ea97d6b64b482d56fb2ca7cf82e0dac4e75f4a6
duong1007 <maybiluyena@gmail.com>
2021-01-11 17:06:06 +0700

    update

--- module_3/bai_2_thiet_ke_va_tao_csdl/bai_tap/xac_dinh_khoa_chinh_va_khoa_ngoai.sql
+++ module_3/bai_2_thiet_ke_va_tao_csdl/bai_tap/xac_dinh_khoa_chinh_va_khoa_ngoai.sql
@@ -1,6 +1,14 @@
+drop database if exists bank_account;
+
 create database bank_account;
 
 use bank_account;
+
+create table customer (
+	customer_number int primary key,
+    full_name varchar(50),
+    address varchar(100)
+);
 
 create table account_type (
 	id int primary key,
@@ -12,30 +20,27 @@ create table `account` (
     account_type_id int,
     `date` 	date,
     balance double,
-    foreign key (account_type_id) references account_type (id)
+	customer_customer_number int unique,
+    
+    foreign key (account_type_id) references account_type (id),
+	foreign key (customer_customer_number) references customer (customer_number)
 );
 
 create table email_of_customer (
 	email_number int primary key,
-    email varchar(50) UNIQUE
+    email varchar(50) UNIQUE,
+    customer_customer_number int unique,
+    
+    foreign key (customer_customer_number) references customer (customer_number)
 );
 
 create table phone_of_customer (
 	phone_number int primary key,
-    phone varchar(50) UNIQUE
-);
-
-create table customer (
-	customer_number int primary key,
-    full_name varchar(50),
-    address varchar(100),
-    email_of_customer_email varchar(50) UNIQUE,
-    phone_of_customer_phone int,
-    account_account_number int,
+    phone varchar(50) UNIQUE,
+    customer_customer_number int unique,
     
-    foreign key (email_of_customer_email) references email_of_customer (email),
-    foreign key (phone_of_customer_phone) references phone_of_customer (phone),
-    foreign key (account_account_number) references `account` (account_number)
+	foreign key (customer_customer_number) references customer (customer_number)
+
 );
 
 
