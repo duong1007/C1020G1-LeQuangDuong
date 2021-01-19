c0d2e69b148af4ace0b1ae73282eff7e1ea0d049
duong1007 <maybiluyena@gmail.com>
2021-01-19 16:49:23 +0700

    update css

--- module_3/case_study_database/yeu_cau_25.sql
+++ module_3/case_study_database/yeu_cau_25.sql
@@ -8,7 +8,7 @@ for each row select count(hop_dong.ID_Hop_Dong) as so_hop_dong_con_lai into @r f
 delimiter ;
 
 delete from hop_dong
-where hop_dong.ID_Hop_Dong = 14;
+where hop_dong.ID_Hop_Dong = 13;
 
 select @r;
 
--- module_3/case_study_database/yeu_cau_26.sql
+++ module_3/case_study_database/yeu_cau_26.sql
@@ -1,16 +1,12 @@
 
 delimiter //
-
+drop trigger if exists Tr_2 //
 create trigger Tr_2 before update on hop_dong
 for each row 
 begin
-if datediff(hop_dong.ngay_ket_thuc,hop_dong.ngay_lam_dich_vu) < 2 then
+if datediff(new.ngay_ket_thuc,old.ngay_lam_dich_vu) < 2 then
 
-set @result = "ngay ket thuc phai lon hon ngay lam hop dong it nhat 2 ngay" ;
-
-
-else
-set @result = "ok" ;
+signal sqlstate "45000" set message_text = "ngày kết thúc hợp đồng phải lớn hơn ngày làm hợp đồng ít nhất là 2 ngày";
 
 end if;
 
